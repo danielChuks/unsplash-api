5481d90bf180457277164f24a3bbae531c8a080b
niealda <danieluchechukwuchuks1@gmail.com>
2022-04-30 20:37:31 +0000

    template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from './Components/Search/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      {'in the app js'}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Search/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Seach = () => {
+  return (
+    <div>
+      Search page 
+    </div>
+  )
+}
+
+
+export default Seach;
