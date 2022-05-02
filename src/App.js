ab115d7166952ceae7b0160b76a781b0a72866ce
niealda <danieluchechukwuchuks1@gmail.com>
2022-05-02 14:44:29 +0000

    state initialization

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <Search />
-      {'in the app js'}
     </div>
   );
 }
--- src/Components/Search/SearchBar.js
+++ src/Components/Search/SearchBar.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import './SearchBar.css'
+
 
 
 class  SearchBar extends React.Component{
-  state ={
-    input: ''
-  }
-conponentDidMount(){
-  this.setState({input: })
+  state = {term: ''}
+ 
+//this function prevent the form from automatically submitting its self.
+onFormSubmit(event){
+  event.preventDefault();
+  console.log(this.term)
 }
 
-  inputConfig(){
-    return(
-      <div>
-         <input onChange={this.state.input} />
-      </div>
-    )
-  
-  }
   render(){
-    return  <div> {this.inputConfig ()} </div>
+    return (
+     <div className='ui segment'> 
+       <form onSubmit={this.onFormSubmit} className='ui form'>
+            <div className='field'>
+            <label>Image search</label>
+              <input type="text"
+                value={this.state.term}
+                // controlled component here we are storing the input value in react not in the Dom. e' represent the event and we assign that event to the state 'term' that assignd the value to the input in react  
+                onChange={e => this.setState({term: e.target.value})}
+               />
+            </div>
+            <button type="Submit">Submit</button>
+          </form>
+        </div>
+    )
   }
 }
 
