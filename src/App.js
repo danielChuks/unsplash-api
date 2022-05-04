4cded599383add823ddb9109af2dd71347cfe14c
niealda <danieluchechukwuchuks1@gmail.com>
2022-05-04 15:45:30 +0000

    fetching data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
-import Search from './Components/Search/SearchBar';
+import SearchBar from './Components/Search/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <Search />
-    </div>
-  );
+
+class App extends React.Component{
+  
+ async onSearchSubmit(term){
+   const response = await axios.get('https://api.unsplash.com/search/photos', {
+        params: {query: term},
+        headers:{
+          Authorization: "Client-ID 3pSdKgPbWo8pqNoN3nbwo1dBEG5kRR1jlkm6fw8rz4o"
+        }
+   })
+   const value = response.data.results
+   console.log(value)
+ }
+  render(){
+    return (
+      <div className="ui Container" style={{marginTop: '10px'}}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    ); 
+  }
+ 
 }
 
 export default App;
--- src/Components/Search/SearchBar.js
+++ src/Components/Search/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './SearchBar.css'
+import './SearchBar.css';
 
 
 
@@ -7,9 +7,10 @@ class  SearchBar extends React.Component{
   state = {term: ''}
  
 //this function prevent the form from automatically submitting its self.
-onFormSubmit(event){
+onFormSubmit = event => {
   event.preventDefault();
-  console.log(this.term)
+
+  this.props.onSubmit(this.state.term); 
 }
 
   render(){
@@ -17,7 +18,7 @@ onFormSubmit(event){
      <div className='ui segment'> 
        <form onSubmit={this.onFormSubmit} className='ui form'>
             <div className='field'>
-            <label>Image search</label>
+            <label>Image search</label>   
               <input type="text"
                 value={this.state.term}
                 // controlled component here we are storing the input value in react not in the Dom. e' represent the event and we assign that event to the state 'term' that assignd the value to the input in react  
