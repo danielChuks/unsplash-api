bc88504fb0a115aeeafbd298dd0daa755ac7789f
Daniel Chuks <80325389+danielChuks@users.noreply.github.com>
2024-12-07 21:24:13 +0100

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers:{
-        Authorization: "Client-ID 3pSdKgPbWo8pqNoN3nbwo1dBEG5kRR1jlkm6fw8rz4o"
+        Authorization: ""
       }
 })
